83469a03786ff8f3953900f4466bb253ec37cf9c
王初阳 <wangchuyang@wangchuyangdeMacBook-Pro.local>
2014-07-14 15:49:59 +0800

    Initial Commit

---
+++ Prim1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Prim1
+//
+//  Created by 王初阳 on 14-7-14.
+//  Copyright (c) 2014年 王初阳. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
